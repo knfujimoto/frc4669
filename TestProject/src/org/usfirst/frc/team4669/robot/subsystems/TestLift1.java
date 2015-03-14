8daa0c87bd4ea6e66cf96581bb3fd01963dead0b
zitan456 <zitan1@galileoweb.org>
2015-03-14 16:05:24 -0700

    Problems with lift1

--- TestProject/src/org/usfirst/frc/team4669/robot/subsystems/TestLift1.java
+++ TestProject/src/org/usfirst/frc/team4669/robot/subsystems/TestLift1.java
@@ -7,26 +7,26 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 /**
  *
  */
-public class TestLift extends TimePos {
+public class TestLift1 extends TimePos {
 	public static double WHEEL_DIA = 1.75;
 	public static double ENC_COUNT = 360;
 
-	public TestLift() {
-		super(3, false); // 4
+	public TestLift1() {
+		super(2, false); // 4
 	}
 
 	public void execute() {
 		long time = System.currentTimeMillis();
 		super.execute(time);
-		SmartDashboard.putNumber("liftFrontPos", drive.getPosition());
-		SmartDashboard.putNumber("liftFrontThrot", drive.getOutputVoltage());
-		SmartDashboard.putNumber("liftFrontErr", drive.getClosedLoopError());
+		SmartDashboard.putNumber("liftRearPos", drive.getPosition());
+		SmartDashboard.putNumber("liftRearThrot", drive.getOutputVoltage());
+		SmartDashboard.putNumber("liftRearErr", drive.getClosedLoopError());
 	}
 	
 	public void moveTo(double inches) {
 		double d = inches / WHEEL_DIA / Math.PI * 360*4;
 		super.moveTo(d);
-		SmartDashboard.putNumber("end", d);
+		SmartDashboard.putNumber("end1", d);
 	}
 	
 }
